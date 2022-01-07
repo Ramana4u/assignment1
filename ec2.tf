ea1ee220af0b17b7f85e7e4bad3ae6c25ed71d66
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-07 10:42:18 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  security_groups = "sg-017c097bb1674f881"
+  key_name = "sshkey1"
+  tags = {
+    Name = "newEC2"
+  }
+}
