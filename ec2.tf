d2266361abdd9f861a5c51c24f1de6af6bcc89a8
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-07 10:47:05 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
-  security_groups = "sg-017c097bb1674f881"
+  security_groups = ["sg-017c097bb1674f881"]
   key_name = "sshkey1"
   tags = {
     Name = "newEC2"
